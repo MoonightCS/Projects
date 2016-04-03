bd97bb61595f19e4cb8fcc137c7f3f2766669ab9
Bogdan <bogdan.popov@mail.ru>
2016-04-04 00:11:12 +0300

    Third project added

--- main.c
+++ main.c
@@ -1,253 +1,78 @@
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+struct List;
+typedef struct List* list;
 
-int ERRRRRROR;
-struct monomial
+struct List
 {
-    long long p, sgn;
-    long long pow;
-    struct monomial *next;
+    int data;
+    list next;
 };
 
-typedef struct monomial monomial;
-
-struct monomial a[70000];
-monomial* begin, *q, *l;
-char s[10000];
-
-void insertstr(monomial *add)
+//even and odd must be !+ NULL
+void split(list *l, list *even, list *odd)
 {
-
-    if (begin == NULL)
-    {
-        begin = add;
-
+    if(l == NULL)
         return;
-    }
-
-    monomial *p = begin;
-    if (add->pow > begin->pow)
+    list it = *l, *evenIt = even, *oddIt = odd;
+    while(it != NULL)
     {
-        add->next = begin;
-        begin = add;
-        return;
-    }
-
-    if (add->pow == begin->pow)
-    {
-        int r = add->sgn*add->p + begin->sgn*begin->p;
-        begin->p = abs(r);
-        begin->sgn = r > 0 ? 1 : -1;//////dddd
-        return;
-    }
-
-    while ((p->next != NULL) && (add->pow > p->next->pow))
-    {
-        p = p->next;
-    }
-
-    if (p->next == NULL)
-    {
-        add->next = NULL;
-        p->next = add;
-    }
-    else
-    {
-        if (add->pow == p->next->pow)
+        if((it)->data % 2 == 0)
         {
-            int r = add->sgn*add->p + p->sgn*p->p;
-            p->p = abs(r);
-            p->sgn = r >= 0 ? 1 : -1;
-            return;
+            *evenIt = it;
+            it = (it)->next;
+            (*evenIt)->next = NULL;
+            evenIt = &(*evenIt)->next;
         }
-        add->next = p->next->next;
-        p->next = add;
+        else // if(-//- == 1)
+        {
+            *oddIt = it;
+            it = (it)->next;
+            (*oddIt)->next = NULL;
+            oddIt = &(*oddIt)->next;
+        }
     }
-    return;
 
 }
 
-monomial *get_monomial(char *x)
+void Print(list l)
 {
-    monomial *tmp = malloc(sizeof(monomial));
-
-    tmp->p = 0;
-    tmp->next = NULL;
-    tmp->pow = 0;
-    tmp->sgn = 1;
-
-    if (x[0] == '\n')
-        return tmp;
-
-    unsigned long len = strlen(x);
-    int pos = 0;
-
-    if (x[0] == '-')
+    while(l != NULL)
     {
-        tmp->sgn = -1;
-        pos++;
+        printf("%d ", l->data);
+        l = l->next;
     }
-    if (x[0] == '+')
-    {
-        tmp->sgn = 1;
-        pos++;
-    }
-
-    long long ms = 0;
-    int fl = 0;
-    while (x[pos] >= '0' && x[pos] <= '9')
-    {
-        fl = 1;
-        ms = ms * 10 + (x[pos] - '0');
-        pos++;
-    }
-    if (ms == 0 && !fl)
-        ms = 1;
-    tmp->p = ms;
-
-    if (pos >= len)
-    {
-        return tmp;
-    }
-
-
-    if (x[pos] !=  'x')
-    {
-        ERRRRRROR = 1;
-        return tmp;
-    }
-    pos++;
-    tmp->pow = 1;
-
-    if (x[pos] == '^')
-    {
-        ms = 0;
-        pos++;
-        fl = 0;
-        while (x[pos] >= '0' && x[pos] <= '9')
-        {
-            fl = 1;
-            ms = ms * 10 + x[pos] - '0';
-            pos++;
-        }
-        if (ms == 0 && !fl)
-            ms = 1;
-        tmp->pow = ms;
-
-    }
-
-    if (x[pos] != '\0')
-    {
-
-        ERRRRRROR = 1;
-    }
-    return tmp;
+    printf("\n");
 }
 
+#include <time.h>
+#include <stdlib.h>
+
+
+list Random(int num)
+{
+    list head = NULL;
+    list *l = &head;
+    while(num > 0)
+    {
+        *l = malloc(sizeof(struct List));
+        (*l)->data = rand() % 20;
+        (*l)->next = NULL;
+        l = &((*l)->next);
+        --num;
+    }
+    return head;
+}
 
 int main(void)
 {
-    //monomial *q, *l;
+    srand(time(NULL));
+    list a = Random(10), b, c;
 
-    int n = 0, p = 0;
-    int c = 0;
-
-
-    for (int i = 0; i < 70000; i++)
-    {
-        a[i].p = 0;
-
-        a[i].pow = 0;
-        a[i].sgn = 0;
-    }
-
-
-    //begin = malloc(sizeof(monomial));
-    //q = begin;
-    //q->next = NULL;
-
-    while ((c = getchar()) != EOF)
-    {
-        if ((c == '-' || c == '+') && p != 0)
-        {
-
-            s[p] = '\0';
-            q = get_monomial(s);
-            insertstr(q);
-            if (ERRRRRROR == 1)
-            {
-                printf("Ошибка");
-                return 1;
-            }
-            n++;
-            p = 0;
-        }
-        if (c != ' ')
-        {
-            s[p] = c;
-            p++;
-        }
-    }
-
-    s[p] = '\0';
-    if (p > 0)
-    {
-
-        q = get_monomial(s);
-        insertstr(q);
-               if (ERRRRRROR == 1)
-        {
-            printf("Ошибка");
-            return 1;
-        }
-
-        n++;
-    }
-    if (n == 0)
-        return 0;
-    //printf("%d %d %d\n", begin->p, begin->pow, begin->sgn);
-    q = begin;
-
-    int fl = 0;
-    //printf("%lld %lld %lld\n", begin->p, begin->pow, begin->sgn);
-    printf("\n");
-    while(q != NULL)
-    {
-        if (q->p == 0)
-        {
-            q=q->next;
-            continue;
-        }
-
-        if (q->sgn > 0 && fl)
-            printf("+");
-        else
-            if (q->sgn < 0 && fl)
-                printf("-");
-            else
-                if (q->sgn < 0)
-                    printf("-");
-        fl = 1;
-        if (q->p > 1)
-            printf("%lld", q->p);
-
-
-        if (q->pow != 0)
-            printf("%c", 'x');
-
-        if (q->pow > 1)
-            printf("^%lld", q->pow);
-        //printf(" ");
-         q = q->next;
-    }
-    if (fl == 0)
-        printf("0");
-
-
-    printf("\n");
-
+    Print(a);
+    split(&a, &b, &c);
+    Print(b);
+    Print(c);
     return 0;
 }
