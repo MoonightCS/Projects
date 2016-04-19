1eda7a4e09c4d2de3b5a0a2e83fbae568ba11dd9
Bogdan <bogdan.popov@mail.ru>
2016-04-19 10:15:52 +0400

    Создание стека

---
+++ Создание стека.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+#define const 10
+
+class stack {
+    
+    char stck[const];
+    int tos;
+    
+public:
+    
+    stack();
+    void push(char ch);
+    char pop();
+};
+
+stack::stack()
+{
+    tos = 0;
+}
+
+void stack::push(char ch)
+{
+    if (tos == const)
+    {
+        cout << "Стек полон";
+        return;
+    }
+    stck[tos] = ch;
+    tos++;
+}
+
+char stack::pop()
+{
+    if (tos == 0)
+    {
+        cout << "Стек пуст";
+        return 0;
+    }
+    tos--;
+    return(stck[tos]);
+}
+
+int main()
+{
+    stack s1, s2;
+    int i;
+    
+    s1.push('a');
+    s2.push('x');
+    s1.push('b');
+    s2.push('y');
+    s1.push('c');
+    s2.push('z');
+    
+    for (i = 0; i < 3; i++)
+        cout << "символ из s1:" << s1.pop() <<"\n";
+    for (i = 0; i < 3; i++)
+        cout << "символ из s2:" << s2.pop() <<"\n";
+    return 0;
+    
+}
