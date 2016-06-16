bfa96b0f29bfd02a9bc304099765350473f249c8
Bogdan Popov <bogdan.popov@mail.ru>
2016-06-16 23:19:45 +0300

    CubeOfSix added

---
+++ Java/CubeOfSix.java
@@ -0,0 +1,32 @@
+public class CubeOfSix {
+
+    public static void main(String[] args) {
+
+        System.out.println(FastCubeRoot(1));
+
+    }
+
+    private static int FastCubeRoot(int number) {
+
+        int firstDigit, secondDigit;
+
+        secondDigit = number % 10;
+        secondDigit = (secondDigit * secondDigit * secondDigit) % 10;
+
+        number /= 1000;
+
+        for (int i = 1; i < 11; i++) {
+
+            if (i * i * i > number) {
+                firstDigit = i - 1;
+                return firstDigit * 10 + secondDigit;
+            }
+
+        }
+
+        return secondDigit;
+
+    }
+
+
+}
