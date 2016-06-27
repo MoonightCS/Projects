466ff22231b38c03242d39fb5df1b5f70965d151
Moonlight <bogdan.popov@mail.ru>
2016-06-27 13:14:39 +0300

    Binary Search modified

--- Java/ArrayBinarySearch.java
+++ Java/ArrayBinarySearch.java
@@ -1,10 +1,22 @@
 
+import java.util.Random;
+
+
 public class ArrayBinarySearch {
 
-    static final int[] arr = {1, 3, 3, 3, 9};
     static final int size = 5;
+    static int[] arr = new int[size];
+
 
     public static void main(String[] args) {
+
+        Random ran = new Random();
+        initArray(arr,ran);
+
+        System.out.println("Отсортированный массив чисел");
+        for (int i: arr)
+            System.out.printf("%d ", i);
+        System.out.println();
 
         System.out.printf("%b %d\n", binarySearch(9), binarySearchInt(9));
         System.out.println(binarySearchInt(4) - binarySearchInt(3)); // Найти количество элементов, равных искомому числу
@@ -56,6 +68,17 @@ public class ArrayBinarySearch {
 
     }
 
+    private static void initArray(int arr[], Random ran) {
+
+        int tmp;
+        arr[0] = 1;
+        for (int i = 1; i < size; i++) {
+
+            while ((tmp = ran.nextInt(15) + 1) < arr[i - 1]);
+            arr[i] = tmp;
+        }
+
+    }
 
 
 
