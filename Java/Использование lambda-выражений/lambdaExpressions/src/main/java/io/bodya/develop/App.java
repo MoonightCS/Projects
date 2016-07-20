b12dd0ede49a3ab3d3dc36b0be41d9813a277c21
Moonlight <bogdan.popov@mail.ru>
2016-07-20 12:53:17 +0300

    Lambda usage updated Added some new examples

--- Java/Использование lambda-выражений/lambdaExpressions/src/main/java/io/bodya/develop/App.java
+++ Java/Использование lambda-выражений/lambdaExpressions/src/main/java/io/bodya/develop/App.java
@@ -1,10 +1,45 @@
 package io.bodya.develop;
 
+import io.bodya.develop.Interfaces.IWorker;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    // Каждое lambda-выражение может быть неявно привязано к какому-нибудь функциональному интерфейсу.
+    public static void execute(IWorker worker) {
+        worker.doSomeWork();
     }
+
+    public static void main( String[] args ) {
+        execute( () -> System.out.println("Worker вызван через Lambda") );
+    }
+
+
+
+    // Использоание lambda в качестве теста для отсеивания чисел в листе/массиве и т.п.
+    public int sumAll(List<Integer> numbers, Predicate<Integer> p) {
+        int total = 0;
+        for (int number : numbers) {
+            if (p.test(number)) {
+                total += number;
+            }
+        }
+        return total;
+    }
+
+    public void testingSomeshit() {
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        sumAll(numbers, n -> true);
+        sumAll(numbers, n -> n % 2 == 0);
+        sumAll(numbers, n -> n > 3);
+
+    }
+
+
+
 }
---
+++ Java/Использование lambda-выражений/lambdaExpressions/src/main/java/io/bodya/develop/Interfaces/IWorker.java
@@ -0,0 +1,8 @@
+package io.bodya.develop.Interfaces;
+
+@FunctionalInterface
+public interface IWorker {
+
+    public void doSomeWork();
+
+}
