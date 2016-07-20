b4b3eb493c1ed9e0d7f98c94a894afa7077c0533
Moonlight <bogdan.popov@mail.ru>
2016-07-20 13:01:16 +0300

    Lambda usage updated Added some new examples

--- Java/Использование lambda-выражений/lambdaExpressions/src/main/java/io/bodya/develop/App.java
+++ Java/Использование lambda-выражений/lambdaExpressions/src/main/java/io/bodya/develop/App.java
@@ -10,18 +10,24 @@ public class App
 {
 
     // Каждое lambda-выражение может быть неявно привязано к какому-нибудь функциональному интерфейсу.
-    public static void execute(IWorker worker) {
+    private static void execute(IWorker worker) {
         worker.doSomeWork();
     }
 
     public static void main( String[] args ) {
         execute( () -> System.out.println("Worker вызван через Lambda") );
+
+        IWorker worker = () -> System.out.println("Worker вызван через Lambda");
+        worker.doSomeWork();
+
+
+        App app = new App();
+        app.testingSomeshit();
     }
 
 
-
     // Использоание lambda в качестве теста для отсеивания чисел в листе/массиве и т.п.
-    public int sumAll(List<Integer> numbers, Predicate<Integer> p) {
+    private int sumAll(List<Integer> numbers, Predicate<Integer> p) {
         int total = 0;
         for (int number : numbers) {
             if (p.test(number)) {
@@ -31,12 +37,12 @@ public class App
         return total;
     }
 
-    public void testingSomeshit() {
+    private void testingSomeshit() {
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        sumAll(numbers, n -> true);
-        sumAll(numbers, n -> n % 2 == 0);
-        sumAll(numbers, n -> n > 3);
+        System.out.println(sumAll(numbers, n -> true));
+        System.out.println(sumAll(numbers, n -> n % 2 == 0));
+        System.out.println(sumAll(numbers, n -> n > 3));
 
     }
 
